b0e0820b093a0916c1bbd94e67d271aa2f45d644
davidba <david.barone@sanitarium.com.au>
2018-05-30 21:13:33 +1000

    Signed-off-by: davidba <david.barone@sanitarium.com.au>

--- Dbarone.Server/WebCommand.cs
+++ Dbarone.Server/WebCommand.cs
@@ -8,13 +8,53 @@ using System.Threading.Tasks;
 using Dbarone.Ioc;
 using System.Web;
 using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+using System.Reflection;
 
 namespace Dbarone.Server
 {
+    /// <summary>
+    /// Base / abstract class for all web controllers
+    /// </summary>
     public abstract class WebCommand : ICommand
     {
         public HttpListenerContext Context { get; set; }
         public IContainer Container { get; set; }
         public abstract string Execute();
+
+        /// <summary>
+        /// A regex expression that defines the routes that are supported.
+        /// The route should not include the domain name. Only the local path
+        /// that the command can handle
+        /// </summary>
+        public abstract string Route { get; }
+
+        /// <summary>
+        /// Returns the first WebCommand object that
+        /// can handle the path of the http request.
+        /// </summary>
+        /// <param name="localPath"></param>
+        /// <returns></returns>
+        public static WebCommand Create(string localPath)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                try
+                {
+                    foreach (var type in assembly.GetTypes().Where(t => typeof(WebCommand).IsAssignableFrom(t) && t.IsClass && !t.IsAbstract))
+                    {
+                        WebCommand cmd = (WebCommand)Activator.CreateInstance(type);
+                        Regex r = new Regex(cmd.Route,RegexOptions.IgnoreCase);
+                        if (r.IsMatch(localPath))
+                            return cmd;
+                    }
+                }
+                catch (ReflectionTypeLoadException)
+                {
+                }
+            }
+            throw new Exception("No WebCommand object found to handle http request path.");
+        }
     }
 }
